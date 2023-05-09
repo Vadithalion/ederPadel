cdd6bb7c2e9d3f8b24fb6367973b17f95c81d629
Iñigo Vadillo <ivadillo6@gmail.com>
2023-05-10 00:16:43 +0200

    create profile component & routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home/home.component';
 import { RegisterComponent } from './components/register/register/register.component';
 import { LoginComponent } from './components/login/login/login.component';
+import { ProfileComponent } from './components/profile/profile/profile.component';
 
 const routes: Routes = [
   {path:'', component: HomeComponent},
   {path:'register', component: RegisterComponent},
-  {path:'login', component: LoginComponent}
+  {path:'login', component: LoginComponent},
+  {path:'profile', component: ProfileComponent}
 ];
 
 @NgModule({
